83af5ee2c08bcf57f86ad03c686da8c3227bd215
Carlos Garcia <carlos.garcia@boatyardx.com>
2023-07-31 10:24:31 -0500

    Create and bind custom books context provider component
    This context will manage the available books state and reading list state

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import FiltersContextProvider from "./context/FiltersContextProvider.jsx";
+import BooksContextProvider from "./context/BooksContextProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FiltersContextProvider>
-      <App />
-    </FiltersContextProvider>
+    <BooksContextProvider>
+      <FiltersContextProvider>
+        <App />
+      </FiltersContextProvider>
+    </BooksContextProvider>
   </React.StrictMode>
 );
